63355e1b499c492e141e1957a5bc28648746e180
Sargon <emanuele.sardini@gmail.com>
2020-11-20 09:03:14 +0100

    added sidebar and icons

---
+++ src/components/Bootstrap/SideBar.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Nav, Col } from "react-bootstrap";
+import { withRouter } from "react-router";
+import { Icon } from 'react-icons-kit';
+import {ic_add} from 'react-icons-kit/md/ic_add';
+import {ic_mode_edit} from 'react-icons-kit/md/ic_mode_edit'
+import './SideBar.css';
+
+const SideBar = ({ page }) => {
+	const [value, setValue] = useState('');
+
+
+	switch(page){
+		case "clients":
+			return(  
+				<>
+					<Col xs={1} id="sidebar-wrapper">
+						<Nav className="col-md-12 d-none d-md-block bg-light sidebar" activeKey="/home" onSelect={selectedKey => alert(`selected ${selectedKey}`)}>
+							<div className="sidebar-sticky"></div>
+							<Nav.Item>
+								<Nav.Link href="/clienti/nuovo"><Icon icon={ic_add} size={40} /></Nav.Link>
+							</Nav.Item>
+							<Nav.Item>
+								<Nav.Link href="/clienti/edit"><Icon icon={ic_mode_edit} size={30} /></Nav.Link>
+							</Nav.Item>							
+						</Nav>
+					</Col>
+
+				</>
+			);
+			break;
+		default: 
+			return(  
+				<>
+					<Col xs={1} id="sidebar-wrapper">
+						<Nav className="col-md-12 d-none d-md-block bg-light sidebar" activeKey="/home" onSelect={selectedKey => alert(`selected ${selectedKey}`)}>
+							<div className="sidebar-sticky"></div>
+							<Nav.Item>
+								<Nav.Link href="/home">Active</Nav.Link>
+							</Nav.Item>
+							<Nav.Item>
+								<Nav.Link eventKey="link-1">Link</Nav.Link>
+							</Nav.Item>
+							<Nav.Item>
+								<Nav.Link eventKey="link-2">Link</Nav.Link>
+							</Nav.Item>
+							<Nav.Item>
+								<Nav.Link eventKey="disabled" disabled>
+									Disabled
+								</Nav.Link>
+							</Nav.Item>
+						</Nav>
+					</Col>
+
+				</>
+			);
+			break;
+
+	}  
+  
+};
+
+export default SideBar;
--- src/containers/Clients/index.js
+++ src/containers/Clients/index.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
-import Query from "../../components/Query";
-import { Row, Col, Container, Form } from 'react-bootstrap';
+import { Row, Col, Container, Form, InputGroup, FormControl } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import Query from "../../components/Query";
 import Client from "../../components/Clients";
+import SideBar from "../../components/Bootstrap/SideBar";
 import ALL_CLIENTS_QUERY from "../../queries/clients/allclients";
+import './clients.css';
 
 const Clients = () => {
   const [value, setValue] = useState('');
+  const [ppvalue, setPPValue] = useState(true);
+  const [lpvalue, setLPValue] = useState(true);
 
+  function onChangePP(e){
+    setPPValue(!ppvalue);
+  }
+
+  function onChangeLP(e){
+    setLPValue(!lpvalue);
+  }
+
+  //manage user login
   var appUser = null;
 
   if(localStorage.getItem(process.env.REACT_APP_LOCALSTORAGE_APPUSER)){
@@ -18,26 +31,50 @@ const Clients = () => {
 
       return (
         <Container fluid>
-          <br />
-          <Row>
-            <Col xs={4}>
-              <Row>
-                <Form.Label column xs="2">Filtra</Form.Label>
-                <Col xs={10}> 
-                  <Form.Control type="text" value={value} onChange={e => setValue(e.target.value)}/>
+          <Row id="row_container">
+            <SideBar page="clients" />
+            
+
+            <Col id="content-wrapper">
+              <br />
+              <Row className="filters-row">
+                <Col>
+                  
+                  <Form>
+                    <Form.Group as={Row} className="filters-form" controlId="clientsFilters">
+                      <Col xs={4}>
+                        
+                        <InputGroup className="filters-inputgroup" size="sm">
+                          <InputGroup.Prepend>
+                            <InputGroup.Text>
+                              Filtra
+                            </InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <FormControl type="text" value={value} onChange={e => setValue(e.target.value)} />
+                        </InputGroup>
+
+                      </Col>
+                      <Col>
+                        <div key="inline-checkbox" className="mb-3">
+                          <Form.Check inline type="checkbox" id="filter-pp" key="filter-pp" label="Persona Fisica" checked={ppvalue} onChange = {e => onChangePP(e)} />
+                          <Form.Check inline type="checkbox" id="filte-lp" key="filter-lp" label="Persona Giuridica" checked={lpvalue} onChange = {e => onChangeLP(e)} />
+                        </div>
+                      </Col>
+                    </Form.Group>
+                  </Form>
+
                 </Col>
-              </Row>  
-            </Col>
-            <Col><Link to={`/clienti/nuovo`}>Nuovo</Link></Col>
-          </Row>
-          <br />
-          <Row>
-            <Col>
-              <Query query={ALL_CLIENTS_QUERY}>
-                {({ data: { clients } }) => {
-                  return <Client data={{clients}}/>;
-                }}
-              </Query>
+              </Row>
+              <br />
+              <Row>
+                <Col>
+                  <Query query={ALL_CLIENTS_QUERY}>
+                    {({ data: { clients } }) => {
+                      return <Client data={{clients}}/>;
+                    }}
+                  </Query>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Container>
