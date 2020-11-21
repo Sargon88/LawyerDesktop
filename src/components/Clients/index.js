674901c1aaa8944fb3f322a40ed817781f389d46
Sargon <emanuele.sardini@gmail.com>
2020-11-21 17:54:30 +0100

    fixed table load

--- src/components/Clients/index.js
+++ src/components/Clients/index.js
@@ -5,11 +5,23 @@ import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 const Clients = ({ data }) => {
 
+
+
+//build plain array
+data.forEach(function(e){
+	//costruire un oggetto apposta sulla base del contenuto dell'array
+	if(e.customer_type == "Fisico"){
+		e.customer_customer = e.customer_customer[0].physical_person;
+
+	} else if(e.customer_type == "Giuridico"){
+		e.customer_customer = e.customer_customer[0].legal_person;
+	}
+
+});
+
 console.log("DATA")
 console.log(data)
 console.log("DATA")
-
-//build plain array
 
 //https://react-bootstrap-table.github.io/react-bootstrap-table2/
 const columns = [{
@@ -20,7 +32,7 @@ const columns = [{
 	  text: 'Nome'
 	},
 	{
-	  dataField: 'customer_customer.physical_person != null ? \"customer_customer.physical_person.pp_surname\" : \"customer_customer.legal_person.lp_name\"',
+	  dataField: 'customer_customer.lp_name || customer_customer.pp_surname',
 	  text: 'Cognome Customer'
 	}];
 
